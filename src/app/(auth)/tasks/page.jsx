e4c0b00c6d1fe52a26c5f2e2d26ce7fa442b7250
Nas <nassersanou23@gmail.com>
2024-10-19 22:53:17 +0200

    task page

--- src/app/(auth)/tasks/page.jsx
+++ src/app/(auth)/tasks/page.jsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/button";
-import { PlusIcon } from "lucide-react";
+import { ArrowLeftIcon } from "@/components/icons/Arrows";
+import { ArrowLeft, PlusIcon } from "lucide-react";
+import Link from "next/link";
 
 const AddNewTask = () =>{
     return  <div className="inline-flex space-x-6 mt-8">
@@ -103,7 +105,7 @@ const sidebarSections = [
   const RenderSidebar = () => {
     return <div>
               <div className="flex justify-between flex h-[60px] items-center md:h-header-height">
-                  <span className="flex" data-state="closed">
+                  {/* <span className="flex" data-state="closed">
                     <button
                       aria-label="Close sidebar"
                       data-testid="close-sidebar-button"
@@ -140,9 +142,9 @@ const sidebarSections = [
                         />
                       </svg>
                     </button>
-                  </span>
+                  </span> */}
                   <div className="flex">
-                    <span className="flex" data-state="closed">
+                    {/* <span className="flex" data-state="closed">
                       <button
                         aria-label="New chat"
                         data-testid="create-new-chat-button"
@@ -162,7 +164,7 @@ const sidebarSections = [
                           />
                         </svg>
                       </button>
-                    </span>
+                    </span> */}
                   </div>
                 </div>
    {sidebarSections.map((section, index) => {
@@ -312,7 +314,13 @@ const createTaskPage = () => {
               </h2> */}
         
                 <nav className="flex h-full w-full flex-col px-3" aria-label="Chat history">
-                    <AddNewTask/>
+                    <div className="px-4 flex grid grid-cols-2  justify-center space-between space-x-4 ">
+                        <Link href="/profile" className="hover:bg-[#333537] h-12 w-12 mt-8 p-2.5  hover:rounded-full ">
+                            <ArrowLeft className="inline-flex space-x-6 w-6 h-6 "/> 
+                        </Link>
+                           <AddNewTask/>
+                    </div>
+             
                       <bard-sidenav  className="bard-sidenav ng-tns-c2350791624-8 ng-trigger ng-trigger-widthTransition ng-star-inserted" 
                             _ngcontent-ng-c1631036836="" 
                             role="navigation" _nghost-ng-c2350791624="" 
@@ -320,6 +328,7 @@ const createTaskPage = () => {
                             jslog="173918;track:impression"
                             style={{"width": "var(--bard-sidenav-open-width);"}}>
                         <div _ngcontent-ng-c1631036836="" className="sidenav-with-history-container ng-star-inserted expanded" >
+                       
                         <RenderSidebar/>
                         </div>
                     </bard-sidenav>
@@ -331,85 +340,7 @@ const createTaskPage = () => {
 
 
       <div className="relative flex h-full max-w-full flex-1 flex-col overflow-hidden">
-        <div className="draggable sticky top-0 z-10 flex min-h-[60px] items-center justify-center border-transparent bg-token-main-surface-primary pl-0 md:hidden">
-         {/*  <div className="no-draggable absolute bottom-0 left-0 top-0 ml-3 inline-flex items-center justify-center">
-            <button
-              type="button"
-              className="inline-flex rounded-md hover:text-token-text-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white active:opacity-50"
-              data-testid="open-sidebar-button"
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon-lg mx-2 text-token-text-secondary"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M3 8C3 7.44772 3.44772 7 4 7H20C20.5523 7 21 7.44772 21 8C21 8.55228 20.5523 9 20 9H4C3.44772 9 3 8.55228 3 8ZM3 16C3 15.4477 3.44772 15 4 15H14C14.5523 15 15 15.4477 15 16C15 16.5523 14.5523 17 14 17H4C3.44772 17 3 16.5523 3 16Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="no-draggable">
-            <button
-              aria-label="Model selector, current model is 4o"
-              type="button"
-              id="radix-:rq:"
-              aria-haspopup="menu"
-              aria-expanded="false"
-              data-state="closed"
-              data-testid="model-switcher-dropdown-button"
-              className="group flex cursor-pointer items-center gap-1 rounded-lg py-1.5 px-3 text-lg hover:bg-token-main-surface-secondary radix-state-open:bg-token-main-surface-secondary font-semibold text-token-text-secondary overflow-hidden whitespace-nowrap"
-            >
-              <div className="text-token-text-secondary">
-                ChatGPT <span className="text-token-text-secondary">4o</span>
-              </div>
-              <svg
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon-md text-token-text-tertiary"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M5.29289 9.29289C5.68342 8.90237 6.31658 8.90237 6.70711 9.29289L12 14.5858L17.2929 9.29289C17.6834 8.90237 18.3166 8.90237 18.7071 9.29289C19.0976 9.68342 19.0976 10.3166 18.7071 10.7071L12.7071 16.7071C12.5196 16.8946 12.2652 17 12 17C11.7348 17 11.4804 16.8946 11.2929 16.7071L5.29289 10.7071C4.90237 10.3166 4.90237 9.68342 5.29289 9.29289Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
-          </div> */}
-          <div className="no-draggable absolute bottom-0 right-0 top-0 mr-3 inline-flex items-center justify-center">
-            <span className="flex" data-state="closed">
-              <button
-                aria-label="New chat"
-                className="h-10 rounded-lg px-2 text-token-text-secondary focus-visible:outline-0 disabled:text-token-text-quaternary focus-visible:bg-token-sidebar-surface-secondary enabled:hover:bg-token-sidebar-surface-secondary"
-              >
-                <svg
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon-xl-heavy"
-                >
-                  <path
-                    d="M15.6729 3.91287C16.8918 2.69392 18.8682 2.69392 20.0871 3.91287C21.3061 5.13182 21.3061 7.10813 20.0871 8.32708L14.1499 14.2643C13.3849 15.0293 12.3925 15.5255 11.3215 15.6785L9.14142 15.9899C8.82983 16.0344 8.51546 15.9297 8.29289 15.7071C8.07033 15.4845 7.96554 15.1701 8.01005 14.8586L8.32149 12.6785C8.47449 11.6075 8.97072 10.615 9.7357 9.85006L15.6729 3.91287ZM18.6729 5.32708C18.235 4.88918 17.525 4.88918 17.0871 5.32708L11.1499 11.2643C10.6909 11.7233 10.3932 12.3187 10.3014 12.9613L10.1785 13.8215L11.0386 13.6986C11.6812 13.6068 12.2767 13.3091 12.7357 12.8501L18.6729 6.91287C19.1108 6.47497 19.1108 5.76499 18.6729 5.32708ZM11 3.99929C11.0004 4.55157 10.5531 4.99963 10.0008 5.00007C9.00227 5.00084 8.29769 5.00827 7.74651 5.06064C7.20685 5.11191 6.88488 5.20117 6.63803 5.32695C6.07354 5.61457 5.6146 6.07351 5.32698 6.63799C5.19279 6.90135 5.10062 7.24904 5.05118 7.8542C5.00078 8.47105 5 9.26336 5 10.4V13.6C5 14.7366 5.00078 15.5289 5.05118 16.1457C5.10062 16.7509 5.19279 17.0986 5.32698 17.3619C5.6146 17.9264 6.07354 18.3854 6.63803 18.673C6.90138 18.8072 7.24907 18.8993 7.85424 18.9488C8.47108 18.9992 9.26339 19 10.4 19H13.6C14.7366 19 15.5289 18.9992 16.1458 18.9488C16.7509 18.8993 17.0986 18.8072 17.362 18.673C17.9265 18.3854 18.3854 17.9264 18.673 17.3619C18.7988 17.1151 18.8881 16.7931 18.9393 16.2535C18.9917 15.7023 18.9991 14.9977 18.9999 13.9992C19.0003 13.4469 19.4484 12.9995 20.0007 13C20.553 13.0004 21.0003 13.4485 20.9999 14.0007C20.9991 14.9789 20.9932 15.7808 20.9304 16.4426C20.8664 17.116 20.7385 17.7136 20.455 18.2699C19.9757 19.2107 19.2108 19.9756 18.27 20.455C17.6777 20.7568 17.0375 20.8826 16.3086 20.9421C15.6008 21 14.7266 21 13.6428 21H10.3572C9.27339 21 8.39925 21 7.69138 20.9421C6.96253 20.8826 6.32234 20.7568 5.73005 20.455C4.78924 19.9756 4.02433 19.2107 3.54497 18.2699C3.24318 17.6776 3.11737 17.0374 3.05782 16.3086C2.99998 15.6007 2.99999 14.7266 3 13.6428V10.3572C2.99999 9.27337 2.99998 8.39922 3.05782 7.69134C3.11737 6.96249 3.24318 6.3223 3.54497 5.73001C4.02433 4.7892 4.78924 4.0243 5.73005 3.54493C6.28633 3.26149 6.88399 3.13358 7.55735 3.06961C8.21919 3.00673 9.02103 3.00083 9.99922 3.00007C10.5515 2.99964 10.9996 3.447 11 3.99929Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              </button>
-            </span>
-          </div>
-        </div>
+         <SecondaryMobileHeader />
         <div className="no-draggable flex w-full items-center justify-center bg-token-main-surface-primary md:hidden" />
         <main className="relative h-full w-full flex-1 overflow-auto transition-width">
           <div
@@ -423,7 +354,7 @@ const createTaskPage = () => {
                   <div className="react-scroll-to-bottom--css-clfmz-1n7m0yu">
              
                     <div className="flex flex-col text-sm md:pb-9">
-                  {/*           <div className="draggable no-draggable-children sticky top-0 p-3 mb-1.5 flex items-center justify-between z-10 h-header-height font-semibold bg-token-main-surface-primary max-md:hidden">
+                            <div className="draggable no-draggable-children sticky top-0 p-3 mb-1.5 flex items-center justify-between z-10 h-header-height font-semibold bg-token-main-surface-primary max-md:hidden">
                         <div className="absolute start-1/2 ltr:-translate-x-1/2 rtl:translate-x-1/2" />
                         <div
                           className="flex items-center gap-0 overflow-hidden"
@@ -513,13 +444,23 @@ const createTaskPage = () => {
                             </div>
                           </button>
                         </div>
-                      </div> */}
+                      </div>
 
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="centered-content flex flex-col text-sm md:pb-9">
+            <div className="zero-state-container ">ZERO STATE CONTAINER 
+             <h1  className="minimized  bard-hello ">
+                <span className="z-20 text-2xl minimized bard-hello-size ng-star-inserted"> 
+                   Hello, Nasser 
+                </span>
+             </h1>
+            </div>        
+          </div>
+                        
             <div className="md:pt-0 dark:border-white/20 md:border-transparent md:dark:border-transparent w-full">
               <div>
                 <div className="m-auto text-base px-3 md:px-4 w-full md:px-5 lg:px-4 xl:px-5">
@@ -653,8 +594,8 @@ const createTaskPage = () => {
                   </div>
                 </div>
                 <div className="relative w-full px-2 py-2 text-center text-xs text-token-text-secondary empty:hidden md:px-[60px]">
-                  <div className="min-h-4">
-                    <div>ChatGPT can make mistakes. Check important info.</div>
+                  <div className="min-h-4 mt-8">
+                    <div>Enter new tasks and save submit to save.</div>
                   </div>
                 </div>
               </div>
@@ -701,4 +642,88 @@ const createTaskPage = () => {
 }
 
 
+export const SecondaryMobileHeader = () => {
+    return   <div className="draggable sticky top-0 z-10 flex min-h-[60px] items-center justify-center border-transparent bg-token-main-surface-primary pl-0 md:hidden">
+    <div className="no-draggable absolute bottom-0 left-0 top-0 ml-3 inline-flex items-center justify-center">
+      <button
+        type="button"
+        className="inline-flex rounded-md hover:text-token-text-primary focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white active:opacity-50"
+        data-testid="open-sidebar-button"
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon-lg mx-2 text-token-text-secondary"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M3 8C3 7.44772 3.44772 7 4 7H20C20.5523 7 21 7.44772 21 8C21 8.55228 20.5523 9 20 9H4C3.44772 9 3 8.55228 3 8ZM3 16C3 15.4477 3.44772 15 4 15H14C14.5523 15 15 15.4477 15 16C15 16.5523 14.5523 17 14 17H4C3.44772 17 3 16.5523 3 16Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+    <div className="no-draggable">
+      <button
+        aria-label="Model selector, current model is 4o"
+        type="button"
+        id="radix-:rq:"
+        aria-haspopup="menu"
+        aria-expanded="false"
+        data-state="closed"
+        data-testid="model-switcher-dropdown-button"
+        className="group flex cursor-pointer items-center gap-1 rounded-lg py-1.5 px-3 text-lg hover:bg-token-main-surface-secondary radix-state-open:bg-token-main-surface-secondary font-semibold text-token-text-secondary overflow-hidden whitespace-nowrap"
+      >
+        <div className="text-token-text-secondary">
+          Give it done <span className="text-token-text-secondary">1.o</span>
+        </div>
+        <svg
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon-md text-token-text-tertiary"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.29289 9.29289C5.68342 8.90237 6.31658 8.90237 6.70711 9.29289L12 14.5858L17.2929 9.29289C17.6834 8.90237 18.3166 8.90237 18.7071 9.29289C19.0976 9.68342 19.0976 10.3166 18.7071 10.7071L12.7071 16.7071C12.5196 16.8946 12.2652 17 12 17C11.7348 17 11.4804 16.8946 11.2929 16.7071L5.29289 10.7071C4.90237 10.3166 4.90237 9.68342 5.29289 9.29289Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+    <div className="no-draggable absolute bottom-0 right-0 top-0 mr-3 inline-flex items-center justify-center">
+      <span className="flex" data-state="closed">
+        <button
+          aria-label="New chat"
+          className="h-10 rounded-lg px-2 text-token-text-secondary focus-visible:outline-0 disabled:text-token-text-quaternary focus-visible:bg-token-sidebar-surface-secondary enabled:hover:bg-token-sidebar-surface-secondary"
+        >
+          <svg
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon-xl-heavy"
+          >
+            <path
+              d="M15.6729 3.91287C16.8918 2.69392 18.8682 2.69392 20.0871 3.91287C21.3061 5.13182 21.3061 7.10813 20.0871 8.32708L14.1499 14.2643C13.3849 15.0293 12.3925 15.5255 11.3215 15.6785L9.14142 15.9899C8.82983 16.0344 8.51546 15.9297 8.29289 15.7071C8.07033 15.4845 7.96554 15.1701 8.01005 14.8586L8.32149 12.6785C8.47449 11.6075 8.97072 10.615 9.7357 9.85006L15.6729 3.91287ZM18.6729 5.32708C18.235 4.88918 17.525 4.88918 17.0871 5.32708L11.1499 11.2643C10.6909 11.7233 10.3932 12.3187 10.3014 12.9613L10.1785 13.8215L11.0386 13.6986C11.6812 13.6068 12.2767 13.3091 12.7357 12.8501L18.6729 6.91287C19.1108 6.47497 19.1108 5.76499 18.6729 5.32708ZM11 3.99929C11.0004 4.55157 10.5531 4.99963 10.0008 5.00007C9.00227 5.00084 8.29769 5.00827 7.74651 5.06064C7.20685 5.11191 6.88488 5.20117 6.63803 5.32695C6.07354 5.61457 5.6146 6.07351 5.32698 6.63799C5.19279 6.90135 5.10062 7.24904 5.05118 7.8542C5.00078 8.47105 5 9.26336 5 10.4V13.6C5 14.7366 5.00078 15.5289 5.05118 16.1457C5.10062 16.7509 5.19279 17.0986 5.32698 17.3619C5.6146 17.9264 6.07354 18.3854 6.63803 18.673C6.90138 18.8072 7.24907 18.8993 7.85424 18.9488C8.47108 18.9992 9.26339 19 10.4 19H13.6C14.7366 19 15.5289 18.9992 16.1458 18.9488C16.7509 18.8993 17.0986 18.8072 17.362 18.673C17.9265 18.3854 18.3854 17.9264 18.673 17.3619C18.7988 17.1151 18.8881 16.7931 18.9393 16.2535C18.9917 15.7023 18.9991 14.9977 18.9999 13.9992C19.0003 13.4469 19.4484 12.9995 20.0007 13C20.553 13.0004 21.0003 13.4485 20.9999 14.0007C20.9991 14.9789 20.9932 15.7808 20.9304 16.4426C20.8664 17.116 20.7385 17.7136 20.455 18.2699C19.9757 19.2107 19.2108 19.9756 18.27 20.455C17.6777 20.7568 17.0375 20.8826 16.3086 20.9421C15.6008 21 14.7266 21 13.6428 21H10.3572C9.27339 21 8.39925 21 7.69138 20.9421C6.96253 20.8826 6.32234 20.7568 5.73005 20.455C4.78924 19.9756 4.02433 19.2107 3.54497 18.2699C3.24318 17.6776 3.11737 17.0374 3.05782 16.3086C2.99998 15.6007 2.99999 14.7266 3 13.6428V10.3572C2.99999 9.27337 2.99998 8.39922 3.05782 7.69134C3.11737 6.96249 3.24318 6.3223 3.54497 5.73001C4.02433 4.7892 4.78924 4.0243 5.73005 3.54493C6.28633 3.26149 6.88399 3.13358 7.55735 3.06961C8.21919 3.00673 9.02103 3.00083 9.99922 3.00007C10.5515 2.99964 10.9996 3.447 11 3.99929Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+      </span>
+    </div>
+  </div>
+}
+
 export default  createTaskPage 
+
+
